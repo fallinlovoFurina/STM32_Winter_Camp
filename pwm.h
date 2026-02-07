acbf3a54a382dd6f68fa8aef9af12c8e92559f7f
luomo233 <157657883@qq.com>
2026-02-07 20:17:43 +0800

    feat(PWM): 新增PWM通道3和4的比较值设置和获取功能
    添加PWM_SetCompare3/PWM_SetCompare4和PWM_GetCompare3/PWM_GetCompare4函数，以支持更多PWM通道的控制

--- pwm.h
+++ pwm.h
@@ -6,8 +6,12 @@
 void PWM_Init(void);
 void PWM_SetCompare1(uint16_t Compare);
 void PWM_SetCompare2(uint16_t Compare);
+void PWM_SetCompare3(uint16_t Compare);
+void PWM_SetCompare4(uint16_t Compare);
 void PWM_Task(void);
 uint16_t PWM_GetCompare1(void);
 uint16_t PWM_GetCompare2(void);
+uint16_t PWM_GetCompare3(void);
+uint16_t PWM_GetCompare4(void);
 
 #endif
